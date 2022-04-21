a4c401c51808a46fdd8a0fe02f45eda59c224408
elysson-simoes-xseed <elysson.simoes@xseed.com.br>
2022-04-20 22:24:29 -0300

    first commit

---
+++ src/main/java/com/ifceppd/redesensores/models/Cliente.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ifceppd.redesensores.models;
+
+import java.util.ArrayList;
+import javax.swing.DefaultListModel;
+
+/**
+ *
+ * @author elysson
+ */
+public class Cliente {
+    private String nome;
+    private DefaultListModel sensores;
+    
+    public Cliente(String nome, DefaultListModel sensores) {
+        this.nome = nome;
+        this.sensores = sensores;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public DefaultListModel getSensores() {
+        return sensores;
+    }
+
+    public void setSensores(DefaultListModel sensores) {
+        this.sensores = sensores;
+    }
+    
+}
---
+++ src/main/java/com/ifceppd/redesensores/models/Sensor.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ifceppd.redesensores.models;
+
+/**
+ *
+ * @author elysson
+ */
+public class Sensor {
+    private String nome;
+    private String paramMonitor;
+    private int limiteMin;
+    private int limiteMax;
+
+    public Sensor(String nome, String paramMonitor, int limiteMin, int limiteMax) {
+        this.nome = nome;
+        this.paramMonitor = paramMonitor;
+        this.limiteMin = limiteMin;
+        this.limiteMax = limiteMax;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getParamMonitor() {
+        return paramMonitor;
+    }
+
+    public void setParamMonitor(String paramMonitor) {
+        this.paramMonitor = paramMonitor;
+    }
+
+    public int getLimiteMin() {
+        return limiteMin;
+    }
+
+    public void setLimiteMin(int limiteMin) {
+        this.limiteMin = limiteMin;
+    }
+
+    public int getLimiteMax() {
+        return limiteMax;
+    }
+
+    public void setLimiteMax(int limiteMax) {
+        this.limiteMax = limiteMax;
+    }
+    
+}
